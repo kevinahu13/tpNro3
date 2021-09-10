68ec35469415bfabf736897ff3076eb1679e875f
Irina Kainer <kainer.irina@gmail.com>
2021-09-10 15:35:48 -0300

    Mejoras

--- cuerpo.php
+++ cuerpo.php
@@ -1,45 +1,61 @@
 <?php
 
 
-class Datos {
+class Datos
+{
     public $nroUno;
     public $nroDos;
     public $resultado;
 
 
 
-  public function __construct($a, $b)
+    public function __construct($a, $b)
     {
         $this->nroUno = $a;
         $this->nroDos = $b;
-        
     }
-    public function elegirCuenta($valor){
-        if ($valor == 0 ) {
-           return $this->sumar();
+    public function elegirCuenta($valor)
+    {
+        if ($valor == 0) {
+            return $this->sumar();
         } else if ($valor == 1) {
             return $this->resta();
-        } else if ($valor == 2){
+        } else if ($valor == 2) {
             return $this->multiplicar();
-        } else  {
+        } else if ($valor == 3) {
             return $this->dividir();
+        } else {
+            return $this->promedio();
         }
     }
 
- public function sumar() {
-    $this->resultado =  $this->nroUno + $this->nroDos;
-    return $this->resultado;
-}
-public function  resta() {
-    $this->resultado = $this->nroUno - $this->nroDos;
-    return $this ->resultado;
-}
-public function  multiplicar() {
-    $this->resultado = $this->nroUno * $this->nroDos;
-    return $this ->resultado;
-}
-public function  dividir() {
-    $this->resultado = $this->nroUno / $this->nroDos;
-    return $this ->resultado;
-}
+    public function sumar()
+    {
+        $this->resultado =  $this->nroUno + $this->nroDos;
+        return $this->resultado;
+    }
+
+    public function resta()
+    {
+        $this->resultado = $this->nroUno - $this->nroDos;
+        return $this->resultado;
+    }
+
+    public function multiplicar()
+    {
+        $this->resultado = $this->nroUno * $this->nroDos;
+        return $this->resultado;
+    }
+
+    public function dividir()
+    {
+        $this->resultado = $this->nroUno / $this->nroDos;
+        return $this->resultado;
+    }
+
+    public function promedio()
+    {
+        $this->resultado = ($this->nroUno + $this->nroDos) / 2;
+        return $this->resultado;
+    }
 }
